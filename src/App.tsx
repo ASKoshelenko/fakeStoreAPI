52487ef3ba93ce67648072a7ee7a68230259e4bb
ASK <39233929+ASKoshelenko@users.noreply.github.com>
2022-09-04 12:26:09 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Product} from './components/Product';
-import {products} from './data/products';
+import {useProducts} from './hooks/products'
+
 
 function App() {
-
+  const { loading, error, products } = useProducts()
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      { loading && <p className="text-center">Wait please. Informations is loading...</p>}
+      { error && <p className="text-center text-red-600"> { error } </p>}
+      { products.map(product =>  <Product product={product} key={product.id} />)  }
     </div>
   );
 
